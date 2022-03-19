497b2c0a63330fa4aba9981e1258f85f951f5fbb
ember <aliomarov.522@mail.ru>
2022-03-19 23:08:03 +0300

    lesson_1

---
+++ service/authors/models.py
@@ -0,0 +1,11 @@
+from uuid import uuid4
+
+from django.db import models
+
+
+class Author(models.Model):
+    uuid = models.UUIDField(primary_key=True, default=uuid4)
+    first_name = models.CharField(max_length=64)
+    last_name = models.CharField(max_length=64)
+    birthday_year = models.PositiveIntegerField()
+    email = models.EmailField(max_length=128, unique=True)
